876e9787672c14407cca8d8f7519d74188ed726e
kwanm <kwanm@LAPTOP-URO86GFS>
2023-06-04 14:12:50 -0600

    Continue class slot

--- Sutham-Connect4-Game/Program.cs
+++ Sutham-Connect4-Game/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Sutham_Connect4_Game
 {
@@ -19,16 +20,7 @@ namespace Sutham_Connect4_Game
         }
 
     }
-    public class TheColumn
-    {
-        List<char> chars;
-        public TheColumn()
-        {
-            chars = new List<char>();    
-
-        }
-
-    }
+ 
 
     public class Tracker
     {
@@ -36,10 +28,76 @@ namespace Sutham_Connect4_Game
 
 
     }
+    public class TheColumn
+    {
+        List<char> chars;
+        public TheColumn()
+        {
+            chars = new List<char>();
+        }
 
-   
+        public void InsertX()
+        {
+            chars.Add( 'X' );
+        }
 
-        
+        public void InsertO()
+        {
+            chars.Add('O');
+        }
+        public void DisplayOne()
+        {
+            if (Checker() == 0)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine('#');
+                }
+            }
+            else if (Checker() == 6)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    //Console.WriteLine(chars[i]);
+                    Console.WriteLine(chars[chars.Count - i - 1]);
+                }
+            }
+            else if(Checker() < 6 )
+            {
+                //int freeSlot = 6 - chars.Count;
+                for (int i = 0; i < 6 - chars.Count; i++)
+                {
+                    Console.WriteLine('#');
+                }
+                for (int i=0; i< chars.Count ;i++)
+                {
+                    Console.WriteLine(chars[chars.Count-i-1]);
+                }
+                
+            }
+           
+        }
+        public int Checker()
+        {
+            if (chars.Count == 6)
+            {
+                return 6;
+            }
+            else { return chars.Count; }
+
+        }
+
+        public override string ToString()
+        {          
+            return chars.ToString();
+        }
+
+    }
+
+
+
+
+
     public class Connect4Game
     {
         //public string Name { get; set; }
@@ -53,15 +111,22 @@ namespace Sutham_Connect4_Game
             { '#', '#', '#', '#', '#', '#', '#' }, { '#', '#', '#', '#', '#', '#', '#' }
             };
 
-        public static List<TheColumn>[] TheBoardList; 
+        public static List<TheColumn> TheBoardList; 
         private static List<Player> playerList;
         public static int turncount = 0;
         static Connect4Game()
         {
             playerList = new List<Player>();
-            TheBoardList = new List<TheColumn>[7];
-        }
-      
+            TheBoardList = new List<TheColumn>();
+            TheBoardList.Add(new TheColumn());
+            TheBoardList.Add(new TheColumn());
+            TheBoardList.Add(new TheColumn());
+            TheBoardList.Add(new TheColumn());
+            TheBoardList.Add(new TheColumn());
+            TheBoardList.Add(new TheColumn());
+            TheBoardList.Add(new TheColumn());
+
+        }      
         public static void AddAPlayer(string name)
         {
             var player = new Player(name);
@@ -71,28 +136,40 @@ namespace Sutham_Connect4_Game
         public bool Checker(char checkNum)
         {
             return true;
-
-
         }
         
         public static bool Play()
         {
             //Console.WriteLine(turncount);
-            if (turncount == 10)
+            if (turncount == 12)
             {
                 return false;
             }
             if (turncount % 2 == 0)
             {
-                Console.WriteLine($"Player {playerList[1]} symbol X please enter row number: ");
-                //Console.WriteLine(turncount);
-                if(TheBoardArr[5, int.Parse(Console.ReadLine())] == '#')
+                Console.WriteLine($"Player {playerList[1]} symbol X please enter column number(1-7): ");
+                int _tempPos = int.Parse(Console.ReadLine());
+                for (int i = 5; i > 0;i-- )
                 {
-                    TheBoardArr[4, int.Parse(Console.ReadLine())] =  'X';
-
+                    //int _tempPos = int.Parse(Console.ReadLine());
+                    if (TheBoardArr[i, _tempPos-1] == '#')
+                    {
+                        TheBoardArr[i, _tempPos-1] =  'X';
+                        turncount++;
+                        DisplayBoard();
+                        return true;
+                    }
+                    else if(TheBoardArr[i, _tempPos-1] == 'X')
+                    {
+                        TheBoardArr[i-1, _tempPos-1] = 'X';
+                        turncount++;
+                        DisplayBoard();
+                        return true;
+                       
+                    }                
                 }
-                turncount++;
-                DisplayBoard();
+                //turncount++;
+                //DisplayBoard();
                 return true;
             }
             else 
@@ -105,52 +182,6 @@ namespace Sutham_Connect4_Game
                 return true;
             }
             //return true;
-
-
-
-            /*
-
-
-            Console.WriteLine("Guess a number: ");
-            int guess = int.Parse(Console.ReadLine());
-            if (guess == _hiddenNo)
-            {
-                playerList[count].Score += 10;
-                Console.WriteLine($"Congratulations {playerList[count].Name}... You guessed it right.");
-                return false;
-            }
-            else if (guess > _hiddenNo)
-            {
-                Console.WriteLine("Guess Lower...");
-            }
-            else
-            {
-                Console.WriteLine("Guess Heigher...");
-            }
-            if (Math.Abs(guess - _hiddenNo) <= 5)
-            {
-                playerList[count].Score += 7;
-            }
-            else if (Math.Abs(guess - _hiddenNo) <= 10)
-            {
-                playerList[count].Score += 5;
-            }
-            else if (Math.Abs(guess - _hiddenNo) <= 15)
-            {
-                playerList[count].Score += 3;
-            }
-            else if (Math.Abs(guess - _hiddenNo) <= 20)
-            {
-                playerList[count].Score += 2;
-            }
-            else
-            {
-                playerList[count].Score += 1;
-            }
-
-            count = (count + 1) % playerList.Count;
-            return true;
-            */
         }
 
         
@@ -173,6 +204,15 @@ namespace Sutham_Connect4_Game
                 Console.WriteLine("|");
             }
             Console.WriteLine("  1  2  3  4  5  6  7 \n");
+
+            Console.WriteLine("Sutham's Connect4Game");
+            Console.WriteLine($" {playerList[0]} VS {playerList[1]}\n");
+
+            
+            TheBoardList[6].InsertX();
+            //TheBoardList[6].InsertO();
+            //char test =
+            TheBoardList[6].DisplayOne();
         }  
 
     } 
